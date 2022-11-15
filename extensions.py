92d832aa987b5b38f9d01f13397b24f59b9309fd
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-11-15 14:33:45 +0300

    pull latest strip

--- extensions.py
+++ extensions.py
@@ -1,8 +1,8 @@
+import dotenv
 import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 from bs4 import BeautifulSoup
-from lxml import html
 
 
 class API:
@@ -16,31 +16,43 @@ class FetchException(Exception):
 
 
 def get_last_strip():
-    base = 'https://www.oglaf.com/'
-    page = 'mrowrowr/'
+    base = 'https://www.oglaf.com'
+    page = update()
+    last_page = dotenv.get_key(find_dotenv(), key_to_get='last_page')
+    if page == last_page:
+        raise FetchException('No updates')
+    else:
+        dotenv.set_key(find_dotenv(), key_to_set='last_page', value_to_set=page)
     url = base + page
     counter = 1
     src = []
     title = []
+    alt = []
+    name = ''
     while True:
         try:
             r = requests.get(url)
             r.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            return src, title
+        except requests.exceptions.RequestException:
+            return src, title, alt, name
         else:
-            htm = r.content
-            soup = BeautifulSoup(htm, 'lxml')
+            soup = BeautifulSoup(r.content, 'lxml')
             strip = soup.find('img', {'id': 'strip'})
+            name = soup.find('title').string
             src.append(strip.attrs['src'])
             title.append(strip.attrs['title'])
+            alt.append(strip.attrs['alt'])
             counter += 1
             url = base + page + str(counter)
 
 
 def update():
     base = 'https://www.oglaf.com/archive/'
-    page = requests.get(base)
-    tree = html.fromstring(page.content)
-    name = tree.xpath('/html/body/div/div[1]/div[1]/a[1]/@href')
-    return name[0]
+    r = requests.get(base)
+    soup = BeautifulSoup(r.content, 'lxml')
+    name = soup.find_all('div')[2].find('a')
+    return name['href']
+
+
+def force_update():
+    dotenv.set_key(find_dotenv(), key_to_set='last_page', value_to_set='')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from aiogram import Bot, Dispatcher, executor, filters, types
+from aiogram import Bot, Dispatcher, executor, types
+
 from extensions import *
 
 bot = Bot(token=API.TOKEN)
@@ -6,19 +7,25 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    src, title = get_last_strip()
-    for page in range(len(src)):
-        await types.ChatActions.upload_photo()
+async def post_strip(message: types.Message):
+    await types.ChatActions.upload_photo()
+    try:
+        src, text, alt, name = get_last_strip()
+    except FetchException as e:
+        await message.answer(str(e))
+    else:
+        await message.answer(name)
         media = types.MediaGroup()
-        media.attach_photo(src[page], title[page])
+        for page in range(len(src)):
+            description = f'"{alt[page]}"\n{text[page]}'
+            media.attach_photo(src[page], description)
         await message.answer_media_group(media=media)
 
 
 @dp.message_handler(commands=['update'])
-async def check_update(message: types.Message):
-    title = update()
-    await message.answer(title)
+async def update(message: types.Message):
+    force_update()
+    await post_strip(message)
 
 
 if __name__ == '__main__':
